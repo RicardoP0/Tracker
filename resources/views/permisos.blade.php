45d22add1b4973c713296cfde74c923636610e2d
Cristian <theberzerker_15_@hotmail.com>
2019-03-19 18:17:15 -0300

    validacion de rut menu admin

--- resources/views/permisos.blade.php
+++ resources/views/permisos.blade.php
@@ -62,7 +62,7 @@
                                 <div class="row md-form mb-5">
                                     <div class="col-md-7">
                                         <label data-error="wrong" data-success="right">Rut</label>
-                                        <input type="text" id="id_rut" name="rut" class="form-control validate" style="margin-bottom: 10px">
+                                        <input type="text" id="id_rut" name="rut" class="form-control validate" oninput="checkRut(this)" style="margin-bottom: 10px">
                                     </div>
                                 </div>
 
@@ -133,6 +133,57 @@
                         </div>
                     </div>
                 </div>
+
+                <!---script de validar rut---->
+                <script>
+                    function checkRut(rut) {
+                        // Despejar Puntos
+                        var valor = rut.value.replace('.','');
+                        // Despejar Guión
+                        valor = valor.replace('-','');
+
+                        // Aislar Cuerpo y Dígito Verificador
+                        cuerpo = valor.slice(0,-1);
+                        dv = valor.slice(-1).toUpperCase();
+
+                        // Formatear RUN
+                        rut.value = cuerpo + '-'+ dv
+
+                        // Si no cumple con el mínimo ej. (n.nnn.nnn)
+                        if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+
+                        // Calcular Dígito Verificador
+                        suma = 0;
+                        multiplo = 2;
+
+                        // Para cada dígito del Cuerpo
+                        for(i=1;i<=cuerpo.length;i++) {
+
+                            // Obtener su Producto con el Múltiplo Correspondiente
+                            index = multiplo * valor.charAt(cuerpo.length - i);
+
+                            // Sumar al Contador General
+                            suma = suma + index;
+
+                            // Consolidar Múltiplo dentro del rango [2,7]
+                            if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+
+                        }
+
+                        // Calcular Dígito Verificador en base al Módulo 11
+                        dvEsperado = 11 - (suma % 11);
+
+                        // Casos Especiales (0 y K)
+                        dv = (dv == 'K')?10:dv;
+                        dv = (dv == 0)?11:dv;
+
+                        // Validar que el Cuerpo coincide con su Dígito Verificador
+                        if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+
+                        // Si todo sale bien, eliminar errores (decretar que es válido)
+                        rut.setCustomValidity('');
+                    }
+                </script>
 
                 <div class="modal fade modalEditClass" id="modEdit" tabindex="-1" role="dialog" aria-hidden="true">
                     <div class="modal-dialog modal-lg" role="document">
